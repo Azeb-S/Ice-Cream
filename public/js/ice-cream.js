c630ad9e05994c1fca0664ed9726820608d4c3a1
Azeb Tesfay <azbseb@gmail.com>
2025-10-16 12:13:08 -0700

    Commit for test

---
+++ public/js/ice-cream.js
@@ -0,0 +1,19 @@
+alert("hello, Ice cream")
+
+document.getElementById('ice-cream').onsubmit = () => {
+ let isValid = true
+ let name = document.getElementById('name').value.trim()
+ if (!name) {
+  document.getElementById('err-name').style.display = "block"
+  isValid = false
+ }
+
+ let email = document.getElementById('email').value.trim()
+ if (!email) {
+  document.getElementById('err-email').style.display = "block"
+  isValid = false
+ }
+
+ return isValid
+
+}
